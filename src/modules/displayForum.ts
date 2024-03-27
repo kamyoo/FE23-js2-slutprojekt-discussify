2acea7ba3ed4d715aeded1c23c8446fd02bdbcea
Anne-LieStahle85 <149506235+Anne-LieStahle85@users.noreply.github.com>
2024-03-27 13:36:23 +0100

    forum1

--- src/modules/displayForum.ts
+++ src/modules/displayForum.ts
@@ -1,10 +1,13 @@
-import { getComments, Com } from "./api.ts";   
+import { postComments, Com, getComments } from "./fetchdata";  
 
-export function displayForm(forumId: string){
-    const formContainer = document.getElementById('formContainer');
-    if (!formContainer) return;
+export async function displayForm(forumId: string){
+    const forum1Container = document.getElementById('forum1Container');
+    if (!forum1Container) return;
 
-    formContainer.innerHTML = '';
+    const comments = await getComments();
+    if(comments.length > 0){
+        comments.forEach(comment => displayComments(comment, forum1Container));
+    }
 
     const forumForm = `
     <form id="${forumId}Form">
@@ -15,11 +18,45 @@ export function displayForm(forumId: string){
         <input type="submit" value="Send" id="sendMsgBtn">
     </form>`;
 
-    formContainer?.insertAdjacentHTML('beforeend', forumForm);
+    forum1Container.innerHTML = forumForm;
+
+    // forum1Container?.insertAdjacentHTML('beforeend', forumForm);
+
+    const form = document.getElementById(`${forumId}Form`) as HTMLElement;
+
+    form?.addEventListener('submit', async(event) =>{
+        event.preventDefault();
+
+        const titleInput = form.querySelector('input[name="title"]') as HTMLInputElement;
+        const messageInput = form.querySelector('textarea[name="message"]') as HTMLTextAreaElement;
+        const title = titleInput.value;
+        const message = messageInput.value;
+
+        const newComment: Com ={
+            title: title,
+            message: message
+        };
+
+        await postComments(newComment);
+
+        titleInput.value = '';
+        messageInput.value ='';
+
+        displayComments(newComment, forum1Container);
+    });
 }
 
-function displayComments(comments: Com[]): void{
-    const container = document.querySelector('#formContainer') as HTMLDivElement;
-}
+function displayComments(comment:Com, container: HTMLElement){
+    const commentDiv = document.createElement('div');
+    commentDiv.classList.add('comment-wrapper');
 
-getComments().then( displayComments )
+    const titleEl = document.createElement('h2');
+    titleEl.textContent = comment.title;
+
+    const messageEl = document.createElement('p');
+    messageEl.textContent = comment.message;
+
+    commentDiv.appendChild(titleEl);
+    commentDiv.appendChild(messageEl);
+    container.appendChild(commentDiv);
+}
